065338c5154eac08c0aa7632e5c3ef628cd6c203
Mohammed Imtiyaz <mohammed.imtiyaz@lighting.com>
2018-04-10 17:06:35 +0530

    add the initial commit

---
+++ src/main/java/com/devopsBuddy/DevopsBuddyApplication.java
@@ -0,0 +1,12 @@
+package com.devopsBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsBuddyApplication.class, args);
+	}
+}

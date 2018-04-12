590f06df9d0cbfbd209890a22b29b5b0907b97c4
Mohammed Imtiyaz <mohammed.imtiyaz@lighting.com>
2018-04-12 18:57:10 +0530

    added logger

--- src/main/java/devopsBuddy/DevopsBuddyApplication.java
+++ src/main/java/devopsBuddy/DevopsBuddyApplication.java
@@ -1,13 +1,19 @@
 package devopsBuddy;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
+
 @SpringBootApplication
 public class DevopsBuddyApplication {
+	private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(DevopsBuddyApplication.class);
 
 	public static void main(String[] args) {
+		LOGGER.info("The class i s statrd");//log check
+		
 		SpringApplication.run(DevopsBuddyApplication.class, args);
 		
 		
--- src/main/java/devopsBuddy/config/i18nConfig.java
+++ src/main/java/devopsBuddy/config/i18nConfig.java
@@ -1,13 +1,18 @@
 package devopsBuddy.config;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
+import devopsBuddy.DevopsBuddyApplication;
+
 @Configuration
 public class i18nConfig {
+	private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(DevopsBuddyApplication.class);
 	@Bean 
 	 public ReloadableResourceBundleMessageSource messageSource() {
+		LOGGER.info("The class i s statrd");
         ReloadableResourceBundleMessageSource resourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
         resourceBundleMessageSource.setBasename("classpath:i18n/messages");
         // Checks for new messages every 30 minutes

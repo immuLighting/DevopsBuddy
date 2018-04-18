7d37f8efa16f5c0b2514b237134390fcb5bc6c8f
Mohammed Imtiyaz <mohammed.imtiyaz@lighting.com>
2018-04-18 17:52:06 +0530

    full tweet text

--- src/main/java/devopsBuddy/web/controllers/SearchControl.java
+++ src/main/java/devopsBuddy/web/controllers/SearchControl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.ListIterator;
 
 import org.springframework.social.twitter.api.SearchParameters;
+import org.springframework.social.twitter.api.SearchParameters.ResultType;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class SearchControl {
@@ -25,18 +27,22 @@ private final TwitterTemplate twitterTemplate;
 	    	
 	    	
 	        SearchResults results = twitterTemplate.searchOperations().search(
-	        		new SearchParameters(search).lang("en"));
+	        		new SearchParameters(search).lang("en").count(200).resultType(ResultType.RECENT));
 	        
 	        List<Tweet> tweets = results.getTweets(); 
 	        ListIterator<Tweet> itr=tweets.listIterator();  
 	        int i=0;
-	       while(itr.hasNext()==true) {
-	    	   processTweetUserId(itr.next().getText());
-	        System.out.println("and tweets read is :"+itr.next().getText());
-	        i++;
-	       }
+	        for(;i<20;i++)
+	        processTweetUserId(tweets.get(i).getUnmodifiedText());
+	 
+	        RestTemplate restTemplate = twitterTemplate.getRestTemplate();
+
+	        //More Query Options @ https://dev.twitter.com/rest/reference/get/search/tweets    
+	        String response = restTemplate.getForObject("https://api.twitter.com/1.1/search/tweets.json?q="+search, String.class);
+	       System.out.println("JSON Response From Twitter: "+response);
 	       
-	       System.out.println("The total number of tweets are :"+i);
+	       
+	       System.out.println("The total number of tweets are :"+tweets.size());
 	        model.addAttribute("tweets", tweets);
 	        model.addAttribute("search", search);
 	        
@@ -44,8 +50,11 @@ private final TwitterTemplate twitterTemplate;
 	    }
 
 	 
-	 public void processTweetUserId(String userID) {
-		 System.out.print("The user id  :"+userID);
+	 public void processTweetUserId(String text) {
+		 System.out.println(text);
+		 String [] words=text.split(" ");
+		 for (int i = 0, l = words.length; i  < l; i++)
+		        System.out.println("the string "+i+" = "+words[i] );
 	 }
 
 }
